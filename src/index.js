85c69b97471852507d23067d63bee874f789e1b2
Isela <isela.v.alarcon@gmail.com>
2021-04-11 22:33:52 +0200

    Updating state properties

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    //THIS IS THE ONLY TIME WE DO DIRECT AN ASSIGMENT: to this.state
     this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //we called setState
+        this.setState({ lat: position.coords.latitude });
+
+        //WE DID NOT, NEVER!!!!! : this.state.lat = position.coords.latitude
+      },
+      err => console.log(err)
+    );
   }
 
   //REACT SAYS WE HAVE TO REACT RENDER
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-
-    return <div>Latitude:</div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
