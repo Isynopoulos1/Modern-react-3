76826ae0fd6a26d390ff3d181306640c942a0426
Isela <isela.v.alarcon@gmail.com>
2021-04-12 20:26:50 +0200

    Conditionally rendering content

--- src/index.js
+++ src/index.js
@@ -22,13 +22,15 @@ class App extends React.Component {
 
   //REACT SAYS WE HAVE TO REACT RENDER
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error:{this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Latitude: {this.state.lat} </div>;
+    }
+
+    return <div> Loading! </div>;
   }
 }
 
