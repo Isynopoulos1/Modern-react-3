2a590142c2aef1b32222f849024e15e10a3cd2a2
Isela <isela.v.alarcon@gmail.com>
2021-04-13 14:33:22 +0200

    alternate state initialization

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    //THIS IS THE ONLY TIME WE DO DIRECT AN ASSIGMENT: to this.state
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
