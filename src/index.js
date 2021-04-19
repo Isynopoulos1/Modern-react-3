efefa14811e7430574a0eb07ac704002a8449ce3
Isela <isela.v.alarcon@gmail.com>
2021-04-19 15:56:44 +0200

    Showing a loading spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   // STATES
@@ -24,7 +25,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div> Loading! </div>;
+    return <Spinner />;
   }
 }
 
